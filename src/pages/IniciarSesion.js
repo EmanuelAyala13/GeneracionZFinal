75b211ee9d11a3ef8f3c214d0fe0d716ea598b74
EmanuelAyala <24siemprels@gmail.com>
2024-02-15 01:01:27 -0300

    first commit

--- src/pages/IniciarSesion.js
+++ src/pages/IniciarSesion.js
@@ -12,6 +12,8 @@ function IniciarSesion() {
   const [password, setPassword] = useState('');
   const [registeredUsers, setRegisteredUsers] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showWelcomeMessage, setShowWelcomeMessage] = useState(false);
+  const [showLogoutConfirmation, setShowLogoutConfirmation] = useState(false); 
 
   useEffect(() => {
     const usersFromLocalStorage = JSON.parse(localStorage.getItem('registeredUsers'));
@@ -26,6 +28,7 @@ function IniciarSesion() {
       setLastName(lastName);
       setEmail(email);
       setIsLoggedIn(true);
+      setShowWelcomeMessage(true);
     }
   }, []);
 
@@ -34,6 +37,7 @@ function IniciarSesion() {
     if (user) {
       toast.success('Inicio de sesión exitoso');
       setIsLoggedIn(true);
+      setShowWelcomeMessage(true);
       localStorage.setItem('loggedInUser', JSON.stringify(user));
     } else {
       toast.error('Correo o contraseña incorrectos.');
@@ -51,13 +55,20 @@ function IniciarSesion() {
       localStorage.setItem('registeredUsers', JSON.stringify(updatedUsers));
       toast.success('Registro exitoso. Iniciando sesión automáticamente.');
       setIsLoggedIn(true);
+      setShowWelcomeMessage(true);
       localStorage.setItem('loggedInUser', JSON.stringify(newUser));
     }
   };
 
   const handleLogout = () => {
+    setShowLogoutConfirmation(true); 
+  };
+
+  const confirmLogout = () => {
     setIsLoggedIn(false);
+    setShowWelcomeMessage(false);
     localStorage.removeItem('loggedInUser');
+    setShowLogoutConfirmation(false); 
   };
 
   const toggleForm = () => {
@@ -66,45 +77,57 @@ function IniciarSesion() {
 
   return (
     <div>
-      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Navbar isLoggedIn={isLoggedIn} />
       <ToastContainer />
-      {isLoggedIn ? (
-        <div className="user-info-container">
-          <h2>Bienvenido, {name} {lastName}</h2>
-          <p>Correo: {email}</p>
-          <button className="logout-button" onClick={handleLogout}>Cerrar sesión</button>
-        </div>
-      ) : (
-        <div className="container"> 
-          {isLogin ? (
-            <div>
-              <h2>Iniciar Sesión</h2>
-              <form className="form-container"> 
-                <input type="email" className="input-field" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" className="input-field" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="button" className="button" onClick={handleLogin}>Iniciar Sesión</button>
-              </form>
-              <div className="signup-link">
-                <p>¿No tienes una cuenta? <button className="link" onClick={toggleForm}>Regístrate aquí</button></p>
+      <div className="container"> 
+        {showWelcomeMessage && (
+          <div className="user-info-container">
+            <h2>Bienvenido: {name} {lastName}</h2>
+            <p>Correo: {email}</p>
+            <div className="logout-container">
+              {showLogoutConfirmation ? ( 
+                <div>
+                  <button className="logout-button" onClick={confirmLogout}>Confirmar</button>
+                  <button className="logout-button" onClick={() => setShowLogoutConfirmation(false)}>Cancelar</button>
+                </div>
+              ) : (
+                <button className="logout-button" onClick={handleLogout}>Cerrar sesión</button>
+              )}
+            </div>
+          </div>
+        )}
+        {!showWelcomeMessage && (
+          <div>
+            {isLogin ? (
+              <div>
+                <h2>Iniciar Sesión</h2>
+                <form className="form-container"> 
+                  <input type="email" className="input-field" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  <input type="password" className="input-field" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  <button type="button" className="button" onClick={handleLogin}>Iniciar Sesión</button>
+                </form>
+                <div className="signup-link">
+                  <p>¿No tienes una cuenta? <button className="link" onClick={toggleForm}>Regístrate aquí</button></p>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div>
-              <h2>Registrarse</h2>
-              <form className="form-container"> 
-                <input type="text" className="input-field" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} />
-                <input type="text" className="input-field" placeholder="Apellido" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                <input type="email" className="input-field" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input type="password" className="input-field" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="button" className="button" onClick={handleRegister}>Registrarse</button>
-              </form>
-              <div className="login-link">
-  <p>¿Ya tienes una cuenta? <button className="link" onClick={toggleForm}>Iniciar sesión aquí</button></p>
-</div>
-            </div>
-          )}
-        </div>
-      )}
+            ) : (
+              <div>
+                <h2>Registrarse</h2>
+                <form className="form-container"> 
+                  <input type="text" className="input-field" placeholder="Nombre" value={name} onChange={(e) => setName(e.target.value)} />
+                  <input type="text" className="input-field" placeholder="Apellido" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                  <input type="email" className="input-field" placeholder="Correo" value={email} onChange={(e) => setEmail(e.target.value)} />
+                  <input type="password" className="input-field" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                  <button type="button" className="button" onClick={handleRegister}>Registrarse</button>
+                </form>
+                <div className="login-link">
+                  <p>¿Ya tienes una cuenta? <button className="link" onClick={toggleForm}>Iniciar sesión aquí</button></p>
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,14 +1,15 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-image: url('/images/FondoPrincipal.png'); 
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    margin: 0;
-    font-family: 'Arial', sans-serif;
-  }
+body {
+  background-image: url('/images/FondoPrincipal.png');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  margin: 0;
+  font-family: 'Arial', sans-serif;
+}
+
 
   h1 {
     background: white; 
